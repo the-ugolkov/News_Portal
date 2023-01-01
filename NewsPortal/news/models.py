d7c2eeee2aba07926e743619e294bee713b5573d
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-01-01 13:39:37 +0800

    Создал модели для дальнейшей настройки проекта

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class PostCategory(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
