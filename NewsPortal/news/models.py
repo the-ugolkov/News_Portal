60040095fb2d48655da55c64843f86d476aab237
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-01-01 15:25:44 +0800

    Описал модели

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from NewsPortal.res import TYPES, article
+from res import TYPES, article
 
 
 class Author(models.Model):
