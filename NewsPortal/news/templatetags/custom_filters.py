4a01d2a2ad8456e1bdf784de81bbc21a970bf2d9
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-01-05 22:30:45 +0800

    реализовал проверку типа обьекта для фильтра

--- NewsPortal/news/templatetags/custom_filters.py
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -5,11 +5,20 @@ from django import template
 register = template.Library()
 
 
+class CensorException(Exception):
+    pass
+
+
 @register.filter()
 def censor(text):
-    non_cens = ['редиска', 'овечкин', 'новость', 'это', 'что', 'год']
-    for word in non_cens:
-        text = re.sub(word, word[0] + '*' * (len(word) - 1), text)
-        title_word = word.title()
-        text = re.sub(title_word, title_word[0] + '*' * (len(word) - 1), text)
-    return text
+    non_cens = ['добавить', 'дату', 'новость', 'все', 'user']
+    try:
+        if not isinstance(text, str):
+            raise CensorException('Error')
+        for word in non_cens:
+            text = re.sub(word, word[0] + '*' * (len(word) - 1), text)
+            title_word = word.title()
+            text = re.sub(title_word, title_word[0] + '*' * (len(word) - 1), text)
+        return text
+    except CensorException as e:
+        print(e)
