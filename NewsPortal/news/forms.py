d5804cd2032de1025e3d96bca69d14a65ca0510c
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-01-19 09:07:07 +0800

    создал группы и реализовал автоматическое добавление при регистрации

--- NewsPortal/news/forms.py
+++ NewsPortal/news/forms.py
@@ -1,4 +1,6 @@
+from allauth.account.forms import SignupForm
 from django import forms
+from django.contrib.auth.models import Group
 
 from .models import Post
 
@@ -7,3 +9,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['category', 'author', 'title', 'text']
+
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='common')
+        basic_group.user_set.add(user)
+        return user
