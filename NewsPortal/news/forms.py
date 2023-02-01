a515883dcfadd2a8e1503eb23cea31d98f0a8618
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-02-01 21:37:25 +0700

    .

--- NewsPortal/news/forms.py
+++ NewsPortal/news/forms.py
@@ -5,6 +5,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 from .models import Post
+# from ..NewsPortal import settings
 
 
 class PostForm(forms.ModelForm):
@@ -19,5 +20,23 @@ class BasicSignupForm(SignupForm):
         user = super(BasicSignupForm, self).save(request)
         basic_group = Group.objects.get(name='common')
         basic_group.user_set.add(user)
+        #
+        # html_content = render_to_string(
+        #     'post_created_email.html',
+        #     {
+        #         'text': "Добро пожаловать на NewsPortal",
+        #         'link': f'{settings.SITE_URL}/news/',
+        #     }
+        # )
+        #
+        # msg = EmailMultiAlternatives(
+        #     subject='Добро пожаловать',
+        #     body='',
+        #     from_email=settings.DEFAULT_FROM_EMAIL,
+        #     to=user.values('email'), #????
+        # )
+        #
+        # msg.attach_alternative(html_content, 'text/html')
+        # msg.send()
 
         return user
