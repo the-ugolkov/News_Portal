5b5843f16ae7398ec0c8ea299978a036337abd78
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-02-09 13:20:16 +0700

    реализована задача по еженедельной рассылке

--- NewsPortal/news/tasks.py
+++ NewsPortal/news/tasks.py
@@ -1,9 +1,11 @@
+import datetime
+
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
-from .models import Post
+from .models import Post, Category
 
 
 @shared_task
@@ -29,3 +31,30 @@ def send_message(oid):
 
             msg.attach_alternative(html_content, 'text/html')
             msg.send()
+
+
+@shared_task
+def every_week_message():
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(time_in__gte=last_week)
+    categories = set(posts.values_list('category__name', flat=True))
+    subscribers = set(Category.objects.filter(name__in=categories).values_list('subscribers__email', flat=True))
+
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
