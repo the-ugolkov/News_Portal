647979d9addd93d5a0e38f540029f3ba209fa2c6
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-02-17 14:59:28 +0700

    реализовал кеширование страниц новостей

--- NewsPortal/news/urls.py
+++ NewsPortal/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import NewDetail, NewsList, SearchList, PostCreate, PostUpdate, PostDelete, CategoryList, subscribe
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('news/', NewsList.as_view(), name='post_list'),
-    path('news/<int:pk>', NewDetail.as_view(), name='post_detail'),
+    path('news/', cache_page(60)(NewsList.as_view()), name='post_list'),
+    path('news/<int:pk>', cache_page(60 * 5)(NewDetail.as_view()), name='post_detail'),
     path('news/search/', SearchList.as_view(), name='post_search'),
     path('news/create/', PostCreate.as_view(), name='post_create'),
     path('news/<int:pk>/edit/', PostUpdate.as_view(), name='post_create'),
