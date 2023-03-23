ec45aac382e2e7da740524022d8b87e36f33c48b
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-03-23 15:21:34 +0700

    реализовал добавление юзера в модель автора при нажатии "Стать автором"

--- NewsPortal/protect/views.py
+++ NewsPortal/protect/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from news.models import Author
+
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'protect/index.html'
@@ -19,4 +21,5 @@ def upgrade_me(request):
     premium_group = Group.objects.get(name='authors')
     if not request.user.groups.filter(name='authors').exists():
         premium_group.user_set.add(user)
+    Author.objects.create(user=user)
     return redirect('/accounts/')
